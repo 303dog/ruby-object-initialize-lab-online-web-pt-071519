4cbe67d32b50803e7745936a300d1afe68e45651
Paul Beckwith <303dog@gmail.com>
2019-08-12 00:49:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,9 @@
 class Dog
   
   
-  def initialize(name, breed)
+  def initialize(name, breed="Mutt")
    @name=name
    @breed=breed
   end
-  
-  def breed(breed)
-    if breed== "nil""
-      puts "@name "mutt""
 
 end
